d5a923ddac86fe85f10b40245adbaef00dda3ce8
Betsegaw Lemma <betsegawlemma@gmail.com>
2019-04-21 12:17:31 +0300

    define layouts

--- app/src/main/java/et/edu/aait/fragmentexamples/Message.kt
+++
@@ -1,5 +0,0 @@
-package et.edu.aait.fragmentexamples
-
-import java.io.Serializable
-
-data class Message(val header:String, val body:String):Serializable
--- app/src/main/java/et/edu/aait/fragmentexamples/ShowMessageFragment.kt
+++ app/src/main/java/et/edu/aait/fragmentexamples/ShowMessageFragment.kt
@@ -6,39 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_show_message.view.*
 
 class ShowMessageFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_show_message, container, false)
-
-        val headerTextView = view.show_header_tv
-        val bodyTextView = view.show_body_tv
-
-        val message = arguments?.getSerializable(MESSAGE) as Message
-
-        headerTextView.text = message.header
-        bodyTextView.text = message.body
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_show_message, container, false)
     }
-
-    companion object {
-
-        fun newInstance(message: Message): ShowMessageFragment {
-
-          val showMessageFragment = ShowMessageFragment()
-          val args = Bundle()
-          args.putSerializable(MESSAGE, message)
-          showMessageFragment.arguments = args
-
-            return showMessageFragment
-        }
-
-    }
-
-
 }
+
+
